b4b4b67653ab398f63be8355ff99330fcdb87650
Melksedeque <freelancer@melksedeque.com.br>
2025-05-01 19:23:29 -0300

    Atualização de arquivos da nova versão do React com Vite

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import 'normalize.css';
+import './index.css';
+import Cardapio from './pages/Cardapio';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Cardapio />
+  </StrictMode>,
+)
