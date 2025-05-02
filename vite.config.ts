64dcde0527aa5e90a5869352932410a68960143c
Melksedeque <freelancer@melksedeque.com.br>
2025-05-02 15:14:44 -0300

    Configuração global de alias para as pastas; Configuração global de SASS de variáveis e de breakpoints

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'styles': path.resolve(__dirname, './src/styles'),
+      'data': path.resolve(__dirname, './src/data'),
+      'assets': path.resolve(__dirname, './src/assets'),
+      'components': path.resolve(__dirname, './src/components'),
+      'pages': path.resolve(__dirname, './src/pages')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "styles/_variaveis.scss" as *;
+          @use "styles/_breakpoints.scss" as *;
+        `
+      }
+    }
+  }
 })
