fcd81fc4f63f194f4b028f33dbf7023df2f139dd
Melksedeque Silva <freelancer@melksedeque.com.br>
2025-05-04 14:58:24 -0300

    Criação de rotas de maneira rustica para exemplificação e ensino

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import 'normalize.css';
 import './index.css';
 import Inicio from './pages/Inicio';
+import Cardapio from './pages/Cardapio';
+
+const componenteAtual = window.location.pathname === '/' ? <Inicio /> : <Cardapio>;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Inicio />
+    {componenteAtual}
   </StrictMode>,
 )
