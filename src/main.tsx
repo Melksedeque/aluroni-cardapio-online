79fba29b1673c70d4ac081134aeda1ad4144e74d
Melksedeque Silva <freelancer@melksedeque.com.br>
2025-05-04 15:09:09 -0300

    Aplicação de React Router na main

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'normalize.css';
 import './index.css';
-import Inicio from './pages/Inicio';
-import Cardapio from './pages/Cardapio';
-
-const componenteAtual = window.location.pathname === '/' ? <Inicio /> : <Cardapio>;
+import BrowserRouter from './routes';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {componenteAtual}
+    <BrowserRouter />
   </StrictMode>,
 )
