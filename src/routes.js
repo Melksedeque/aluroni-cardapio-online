fd824ad1aa46ef76bdf3cf0bd7046fa2da9fae5f
Melksedeque Silva <freelancer@melksedeque.com.br>
2025-05-04 15:09:31 -0300

    Criação de Rota de Inicio e Cardapio

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,12 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function AppRouter() {
     return (
-
+        <BrowserRouter>
+            <Routes>
+                <Route path='/'element={<Inicio />} />
+                <Route path='/cardapio'element={<Cardapio />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
