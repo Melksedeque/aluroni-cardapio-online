f37a2879dc5f8e90c970c8a36d3332e46a01e010
Melksedeque <freelancer@melksedeque.com.br>
2025-05-06 12:41:14 -0300

    Importação de Componente de Menu nas Rotas ao inves de em cada página

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,12 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Inicio from './pages/Inicio';
 import Cardapio from './pages/Cardapio';
+import Menu from './components/Menu';
 
 export default function AppRouter() {
     return (
         <BrowserRouter>
+             <Menu />
             <Routes>
                 <Route path='/' element={<Inicio />} />
                 <Route path='/cardapio' element={<Cardapio />} />
