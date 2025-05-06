2bb2cfdb07f464cdeadec29e90d0cceadc0bb619
Melksedeque <freelancer@melksedeque.com.br>
2025-05-06 12:48:28 -0300

    Mapeamento dos links do menu segundo o array de rotas

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,9 +2,26 @@ import styles from './Menu.module.scss'
 import logo from 'assets/logo.svg';
 
 export default function Menu() {
+    const rotas = [{
+        label: "Inicio",
+        to: "/"
+    }, {
+        label: "Cardápio",
+        to: "/cardapio"
+    }, {
+        label: "Sobre nós",
+        to: "/sobre"
+    }]
     return (
-    <nav className={styles.menu}>
+      <nav className={styles.menu}>
         <img src={logo} alt="Logo Aluroni" />
+        <ul className={styles.menu__list}>
+            {rotas.map((rota, index) => (
+                <li key={index} className={styles.menu__link}>
+                    <a href={rota.to}>{rota.label}</a>
+                </li>
+            ))}
+        </ul>
       </nav>
-    )
+    );
 }
