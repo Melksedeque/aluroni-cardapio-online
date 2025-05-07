b6f3ac216b9f05a2b385967412f547092bcd7e42
Melksedeque <freelancer@melksedeque.com.br>
2025-05-07 14:39:33 -0300

    Importar o componente de página padrão

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Inicio from './pages/Inicio';
 import Cardapio from './pages/Cardapio';
 import Menu from './components/Menu';
+import PaginaPadrao from './components/PaginaPadrao';
 
 export default function AppRouter() {
     return (
