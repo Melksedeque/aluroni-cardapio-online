bf46f3423f7c11f32ae58ec85b03dbb2399e9862
Melksedeque <freelancer@melksedeque.com.br>
2025-05-10 13:08:11 -0300

    Correção de Rota404 para não exibir o Header

--- src/routes.jsx
+++ src/routes.jsx
@@ -17,8 +17,8 @@ export default function AppRouter() {
                         <Route index element={<Inicio />} />
                         <Route path='cardapio' element={<Cardapio />} />
                         <Route path='sobre' element={<Sobre />} />
-                        <Route path='*' element={<NotFound />} />
                     </Route>
+                    <Route path='*' element={<NotFound />} />
                 </Routes>
                 <Footer />
             </BrowserRouter>
