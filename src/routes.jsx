17e08c4d838980fd44ab6c9128807d0fe558ae63
Melksedeque <freelancer@melksedeque.com.br>
2025-05-10 13:05:03 -0300

    Criação de rota para páginas 404

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Inicio from './pages/Inicio';
 import Cardapio from './pages/Cardapio';
 import Sobre from './pages/Sobre';
+import NotFound from './pages/NotFound';
 import Menu from './components/Menu';
 import Footer from './components/Footer';
 import PaginaPadrao from './components/PaginaPadrao';
@@ -16,6 +17,7 @@ export default function AppRouter() {
                         <Route index element={<Inicio />} />
                         <Route path='cardapio' element={<Cardapio />} />
                         <Route path='sobre' element={<Sobre />} />
+                        <Route path='*' element={<NotFound />} />
                     </Route>
                 </Routes>
                 <Footer />
