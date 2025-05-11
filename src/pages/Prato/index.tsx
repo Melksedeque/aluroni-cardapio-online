44df1cd04f734cdd944341476b8d2df96bfb5d89
Melksedeque <freelancer@melksedeque.com.br>
2025-05-11 10:43:32 -0300

    Refatoração de Tags para um componente próprio

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -21,23 +21,6 @@ export default function Prato() {
           <p className={styles.conteudo__descricao}>
             {prato.description}
           </p>
-          <div className={styles.tags}>
-            <div className={classNames({
-              [styles.tags__tipo]: true,
-              [styles[`tags__tipo__${prato.category.label.toLowerCase()}`]]: true,
-            })}>
-              {prato.category.label}
-            </div>
-            <div className={styles.tags__porcao}>
-              {prato.size}g
-            </div>
-            <div className={styles.tags__qtdpessoas}>
-              Serve {prato.serving} pessoa{prato.serving === 1 ? '' : 's'}
-            </div>
-            <div className={styles.tags__valor}>
-              R$ {prato.price ? prato.price.toFixed(2).replace('.', ',') : ''}
-            </div>
-          </div>
         </div>
       </section>
     </>
