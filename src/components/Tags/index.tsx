dd47eb55600049c99881521ee92da7a6ed925a9e
Melksedeque <freelancer@melksedeque.com.br>
2025-05-11 10:43:10 -0300

    Criação de componente Tags

---
+++ src/components/Tags/index.tsx
@@ -0,0 +1,21 @@
+export default function Tags() {
+    return (
+        <div className={styles.tags}>
+            <div className={classNames({
+              [styles.tags__tipo]: true,
+              [styles[`tags__tipo__${prato.category.label.toLowerCase()}`]]: true,
+            })}>
+              {prato.category.label}
+            </div>
+            <div className={styles.tags__porcao}>
+              {prato.size}g
+            </div>
+            <div className={styles.tags__qtdpessoas}>
+              Serve {prato.serving} pessoa{prato.serving === 1 ? '' : 's'}
+            </div>
+            <div className={styles.tags__valor}>
+              R$ {prato.price ? prato.price.toFixed(2).replace('.', ',') : ''}
+            </div>
+        </div>
+    )
+}
