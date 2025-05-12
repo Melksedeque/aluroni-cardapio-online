e5ae5614e09857f6761e03d7d29d8dfe96c0f205
Melksedeque <freelancer@melksedeque.com.br>
2025-05-12 14:05:05 -0300

    Adição de componente NotFound caso não haja Prato

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -2,11 +2,13 @@ import styles from './Prato.module.scss';
 import { useNavigate, useParams } from 'react-router-dom'
 import cardapio from 'data/cardapio';
 import Tags from 'components/Tags';
+import NotFound from 'pages/NotFound';
 
 export default function Prato() {
   const navigate = useNavigate();
   const { id } = useParams();
   const prato = cardapio.find(item => item.id === Number(id));
+  if(!prato) return (<NotFound />);
   return (
     <>
       <button className={styles.voltar} onClick={() => {navigate(-1)}}>
