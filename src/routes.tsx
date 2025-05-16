7f85ef9c27c0ba2e82dfbcc3d7d4b487806a4336
Melksedeque <freelancer@melksedeque.com.br>
2025-05-16 17:32:22 -0300

    Usando lazy para importar de forma dinamica a página de Prato

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Prato from './pages/Prato';
 import Menu from './components/Menu';
 import Footer from './components/Footer';
 import { lazy, Suspense } from 'react';
@@ -7,8 +6,9 @@ import { lazy, Suspense } from 'react';
 const PaginaPadrao = lazy(() => import('./components/PaginaPadrao'));
 const Cardapio = lazy(() => import('./pages/Cardapio'));
 const Inicio = lazy(() => import('./pages/Inicio'));
-const Sobre = lazy(() => import('./pages/Sobre'));
 const NotFound = lazy(() => import('./pages/NotFound'));
+const Sobre = lazy(() => import('./pages/Sobre'));
+const Prato = lazy(() => import('./pages/Prato'));
 
 export default function AppRouter() {
   return (
